3f774ae66584279e791d760c3b8096c74a0182c8
Shahzaib <shahzaib93@github.com>
2025-10-20 01:39:10 +0500

    Add missing AppDelegate.swift and ViewController.swift for iOS build

---
+++ ios/App/App/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Capacitor
+
+class ViewController: CAPBridgeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
